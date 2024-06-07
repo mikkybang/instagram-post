57166232a4c6b46df51f14a5ba977d918dcc98b7
Michael Efemena <mefemena@gmail.com>
2024-06-07 23:52:18 +0100

    chore: switch graphql module to use async

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,20 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { PostModule } from './post/post.module';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      useFactory: () => ({
+        autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+        playground: false,
+        plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      }),
     }),
+    PostModule,
   ],
 })
 export class AppModule {}
