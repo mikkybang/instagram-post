3c2a4c768d31f46cc449dd6e9052ff4dfcf7605b
Michael Efemena <mefemena@gmail.com>
2024-06-08 08:15:18 +0100

    chore: import post and post media repository in post service

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePostInput } from './dto/create-post.input';
 import { UpdatePostInput } from './dto/update-post.input';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Post, PostMedia } from './entities/post.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PostService {
+  constructor(
+    @InjectRepository(Post) private readonly postRepository: Repository<Post>,
+    @InjectRepository(PostMedia)
+    private readonly postMediaRepository: Repository<PostMedia>,
+  ) {}
+
   create(createPostInput: CreatePostInput) {
     return 'This action adds a new post';
   }
