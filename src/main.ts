eafde62a09a7a4f6704aadf4cd4eb2b6f6ebdb18
Michael Efemena <mefemena@gmail.com>
2024-06-08 08:17:28 +0100

    chore: add validation pipe and use env port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const port: number = Number(`${process.env.PORT}`) || 3000;
+  await app.listen(port);
 }
 bootstrap();
