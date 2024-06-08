a6090a103c5eca79444c382eee92456b4dcdc678
Michael Efemena <mefemena@gmail.com>
2024-06-08 07:58:16 +0100

    feat: add Post and Post Media entity

--- src/post/entities/post.entity.ts
+++ src/post/entities/post.entity.ts
@@ -1,7 +1,56 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int, ID } from '@nestjs/graphql';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Post {
   @Field(() => Int, { description: 'Example field (placeholder)' })
   exampleField: number;
+
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => ID, { description: 'Post Id' })
+  id: string;
+
+  @Column()
+  userId: string;
+
+  @Column()
+  caption?: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @OneToMany(() => PostMedia, (media) => media.post)
+  media: PostMedia;
+}
+
+export class PostMedia {
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => ID, { description: 'Post Media Id' })
+  id: string;
+
+  @Column()
+  @Field(() => String)
+  url: string;
+
+  @Column()
+  @Field(() => String)
+  type: string;
+
+  @Column()
+  postId: string;
+
+  @ManyToOne(() => Post, { eager: true })
+  post: Post[];
 }
